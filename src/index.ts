08623c84a01e4f47a8f3a57f03a4fef10e9c9c15
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-04 11:59:45 -0500

    remove octokit

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,24 @@ function getPath(request: Request): Resource | undefined {
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const { Octokit } = await import('octokit');
-		const octokit = new Octokit({ auth: env.GITHUB_API_KEY, userAgent: 'cloudflare-worker' });
+		// const { Octokit } = await import('octokit');
+		// const octokit = new Octokit({ auth: env.GITHUB_API_KEY, userAgent: 'cloudflare-worker' });
 
-		const {
-			data: { login },
-		} = await octokit.rest.users.getAuthenticated();
+		// const {
+		// 	data: { login },
+		// } = await octokit.rest.users.getAuthenticated();
 
-		return new Response(`Hello, ${login}!`, { status: 200 });
+		// return new Response(`Hello, ${login}!`, { status: 200 });
 
-		// try {
-		// 	const path = getPath(request);
-		// 	return path ? getResource(path, env) : new Response(JSON.stringify({ error: 'Not Found' }), { status: 404 });
-		// } catch (error) {
-		// 	if (error instanceof Error) {
-		// 		return new Response(error.message, { status: 500 });
-		// 	} else {
-		// 		return new Response('An unknown error occurred', { status: 500 });
-		// 	}
-		// }
+		try {
+			const path = getPath(request);
+			return path ? getResource(path, env) : new Response(JSON.stringify({ error: 'Not Found' }), { status: 404 });
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Response(error.message, { status: 500 });
+			} else {
+				return new Response('An unknown error occurred', { status: 500 });
+			}
+		}
 	},
 } satisfies ExportedHandler<Env>;
