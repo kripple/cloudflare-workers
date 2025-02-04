dac49d3322431030327d5f67eb27bf84ee5b8a0c
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-04 12:29:07 -0500

    refactor url utils

--- src/url.ts
+++ src/url.ts
@@ -5,7 +5,7 @@
  * @returns The first part of the path if it exists, otherwise `undefined`.
  */
 export function getPathResource(url: string): string | undefined {
-  const parsedUrl = new URL(url);
-  const pathParts = parsedUrl.pathname.split('/').filter(part => part.length > 0);
-  return pathParts.length > 0 ? pathParts[0] : undefined;
+	const pathname = new URL(url).pathname;
+	const parts = pathname.split('/').filter((part) => part.length > 0);
+	return parts.length > 0 ? parts[0] : undefined;
 }
