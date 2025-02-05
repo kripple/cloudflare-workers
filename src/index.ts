d57a49498d0eed11431b93331e41a79270f1e3c7
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-05 11:39:19 -0500

    update index handler

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-// import { getRoute } from './routes';
-// import { getProfile } from './handlers/profile';
-// import { getLanguages } from './handlers/languages';
-// import { getRepos } from './handlers/repos';
+import { getRoute } from './routes';
+import { getProfile } from './handlers/profile';
+import { getLanguages } from './handlers/languages';
+import { getRepos } from './handlers/repos';
 
 export default {
 	async fetch(request, env, _ctx): Promise<Response> {
-		return new Response(JSON.stringify({ requestUrl: request.url }), { status: 200 });
-		// try {
-		// 	const route = getRoute(request);
-		// 	switch (route) {
-		// 		case 'languages': {
-		// 			return getLanguages(request, env);
-		// 		}
-		// 		case 'profile': {
-		// 			return getProfile(env);
-		// 		}
-		// 		case 'repos': {
-		// 			return getRepos(request, env);
-		// 		}
-		// 		default: {
-		// 			return new Response(JSON.stringify({ error: 'Not Found' }), { status: 404 });
-		// 		}
-		// 	}
-		// } catch (error) {
-		// 	if (error instanceof Error) {
-		// 		return new Response(error.message, { status: 500 });
-		// 	} else {
-		// 		return new Response('An unknown error occurred', { status: 500 });
-		// 	}
-		// }
+		try {
+			const route = getRoute(request);
+			switch (route) {
+				case 'languages': {
+					return getLanguages(request, env);
+				}
+				case 'profile': {
+					return getProfile(env);
+				}
+				case 'repos': {
+					return getRepos(request, env);
+				}
+				default: {
+					return new Response(JSON.stringify({ error: 'Not Found' }), { status: 404 });
+				}
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				return new Response(error.message, { status: 500 });
+			} else {
+				return new Response('An unknown error occurred', { status: 500 });
+			}
+		}
 	},
 } satisfies ExportedHandler<Env>;
